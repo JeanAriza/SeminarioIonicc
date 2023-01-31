fcb858e591c0b75dacd6044cc936dc24f40db440
PROMOVIL ACER <jeanpierre20021109@gmail.com>
2023-01-31 01:53:20 -0500

    Actividad 13/01/2023 Crear Intro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'intro',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'intro',
+    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
   },
 ];
 
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  slideOpt = {
+    initialSlide: 0, //slide inicial (primero) [0,1,2,3]
+    slidesPerView: 1, //configuramos un slide por vista
+    centerSlides: true, //que las slides enten centradas
+    speed: 400 //velocidad movimiento de los slides
+  }
+
+  constructor(private router: Router) { }
+
+  finish(){
+    this.router.navigateByUrl("/home");
+  }
+  ngOnInit() {
+  }
+
+}
