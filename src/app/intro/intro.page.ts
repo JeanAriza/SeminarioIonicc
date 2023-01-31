63fe0da656e2eff9d9539fc13895ac9f7161ddd7
PROMOVIL ACER <jeanpierre20021109@gmail.com>
2023-01-31 02:25:48 -0500

    Actividad 17/01/2023 Login Email Password

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -16,6 +16,33 @@ export class IntroPage implements OnInit {
     speed: 400 //velocidad movimiento de los slides
   }
 
+  slides = [
+    {
+      title: "BlueBook",
+      img: "../../assets/icon/Logo Libreria.png",
+      description: "BlueBook te trae los mejores nuevos títulos seleccionados por nuestros editores. selección exclusiva de ficción contemporánea te ayuda a conectarte con historias absorbentes fuera de tu entorno."
+    },
+    {
+      title: "BlueBook",
+      desc: "¿Te interesa estar al día con el contexto político actual? Nuestra selección de libros electrónicos sobre política ofrece tanto intriga como conocimiento.",
+      subtitle: "Suscribete",
+      description: "Con una suscripción a los libros electrónicos de BlueBook, puedes acceder a esos títulos y más mediante un navegador web o un dispositivo Android o iOS.",
+      img: "assets/logo.png",
+    },
+    {
+      desc: null,
+      subtitle: "BlueBook",
+      img: "../../assets/icon/Logo Libreria.png",
+      description: " Si te interesa más explorar un nuevo pasatiempo o mejorar uno, te recomendamos explorar nuestros libros electrónicos sobre cocina, alimentos y vinos, así como nuestros títulos sobre el hogar y el jardín"
+    },
+    {
+      title: "BlueBook",
+      img: "../../assets/icon/Logo Libreria.png",
+      desc: " Nuestra amplia selección de libros electrónicos populares en verdad tiene algo para todos. Los títulos más vendidos sobre la superación personal —de personas como Mark Manson, Rachel Hollis, Stephen R. Covey y más—",
+      icon: "close"
+    },
+  ]
+
   constructor(private router: Router, private storage: Storage) { }
 
   finish(){
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  loginForm: FormGroup;
+  validation_message = {
+    email: [
+      { type: "required", message: "Campo Obligatorio" },
+      { type: "pattern", message: "Email Invalido" }
+    ],
+    password: [
+      { type: "required", message: "Campo Obligatorio" },
+      { type: "minLength", message: "Password Invalida" }
+    ]
+  }
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        ])
+      ),
+      password: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5)
+        ])
+      )
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  loginUser(credentials: any) {
+    console.log(credentials);
+  }
+
+}
